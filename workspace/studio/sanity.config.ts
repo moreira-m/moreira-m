6d1594fd27a303c68bc48cdb209e31eba3e66d41
Matheus Moreira <69878348+moreira-m@users.noreply.github.com>
2026-02-07 10:08:27 -0300

    feat: add dashboard to build site on netlify

--- workspace/studio/sanity.config.ts
+++ workspace/studio/sanity.config.ts
@@ -4,6 +4,8 @@ import { visionTool } from '@sanity/vision'
 import { presentationTool } from 'sanity/presentation'
 import { schemaTypes } from './schemaTypes'
 import { structure } from './structure'
+import { dashboardTool } from '@sanity/dashboard'
+import { netlifyWidget } from 'sanity-plugin-dashboard-widget-netlify'
 
 export default defineConfig({
   name: 'default',
@@ -20,6 +22,22 @@ export default defineConfig({
       previewUrl: process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000',
     }),
     visionTool(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+          title: 'Netlify Deploys',
+          sites: [
+            {
+              title: 'Portfolio Website',
+              apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID || '',
+              buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK_ID || '',
+              name: 'moreira-m',
+              url: process.env.SANITY_STUDIO_NETLIFY_URL || '',
+            },
+          ],
+        }),
+      ],
+    })
   ],
 
   schema: {
